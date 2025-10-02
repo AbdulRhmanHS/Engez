a38153171626a90af4ce442178a8762a2e843261
AbdulRhman Hassn <bodahassan12@gmail.com>
2025-10-03 00:15:48 +0300

    Add projects and make their name changeable

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import * as tasks from "./tasks"
 
 const input = document.getElementById("task-input");
 const addTaskButton = document.getElementById("add-task");
-const taskList = document.querySelector(".task-list");
+const newProjectButton = document.getElementById("new-project");
+const taskArea = document.querySelector(".task-area");
 
 function createTaskElement(task) {
   const taskElement = document.createElement("li");
@@ -15,16 +16,45 @@ function createTaskElement(task) {
   taskElement.appendChild(checkBox);
   taskElement.appendChild(document.createTextNode(" " + task.name));
 
-  taskList.appendChild(taskElement);
+  taskArea.appendChild(taskElement);
+}
+
+function createProjectElement(project) {
+  const projectElement = document.createElement("div");
+  projectElement.classList.add("project");
+
+  const projectName = document.createElement("p");
+  projectName.textContent = project.name;
+  projectName.contentEditable = true;
+  project.changeableName(projectName);
+  projectName.classList.add("project-name");
+
+  const projectBody = document.createElement("div");
+  projectBody.classList.add("project-body");
+
+  projectElement.append(projectName, projectBody);
+  taskArea.appendChild(projectElement);
 }
 
 function addTasktoScreen() {
-  const task = tasks.addTask(input.value);
-  createTaskElement(task);
-  input.value = "";
+  if (input.value == "") {
+    return;
+  }
+  else {
+    const task = tasks.addTask(input.value);
+    createTaskElement(task);
+    input.value = "";
+  }
+}
+
+function addProjecttoScreen() {
+  const project = tasks.addProject("Default Project");
+  createProjectElement(project);
 }
 
 addTaskButton.addEventListener("click", addTasktoScreen); // Add task when button is clicked
+
+newProjectButton.addEventListener("click", addProjecttoScreen) // Add a new project
 
 input.addEventListener("keydown", (event) => { // Add task when enter is pressed
   if (event.key == "Enter") {
--- src/tasks.js
+++ src/tasks.js
@@ -1,3 +1,36 @@
+class project {
+    constructor(name) {
+        this.name = name;
+        this.tasks = [];
+    }
+
+    appendTask(task) {
+        this.tasks.push(task);
+    }
+
+    changeableName(projectNameElement) {
+        
+        // Change name with typing
+        projectNameElement.addEventListener("input", (event) => {
+            this.name = event.target.textContent
+        });
+
+        // Reset the default name when empty
+        projectNameElement.addEventListener("blur", () => {
+            if (this.name === "") {
+            this.name = "Default Project";
+            projectNameElement.textContent = this.name;
+            }
+        });
+
+        // Prevent enter key from making a new line
+        projectNameElement.addEventListener("keydown", (event) => {
+            if (event.key === "Enter") {
+            event.preventDefault();
+            }
+        });
+    }
+}
 class task {
     constructor(name) {
         this.name = name
@@ -13,4 +46,9 @@ export function addTask(name) {
         const newTask = new task(name);
         return newTask;
     }
+}
+
+export function addProject(name) {
+    const newProject = new project(name);
+    return newProject;
 }
